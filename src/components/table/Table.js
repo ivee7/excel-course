7281db0a987b9b7f492b430078cf45d9735deeb1
ivee7 <ivanman007@mail.ru>
2021-10-18 18:36:12 +0300

    finish redux and table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -33,7 +33,6 @@ export class Table extends ExcelComponent {
         this.selectCell(this.$root.find('[data-id="0:0"]'))
 
         this.$on('formula:input', value => {
-            console.log(value)
             this.selection.current
                 .attr('data-value', value)
                 .text(parse(value))
@@ -57,7 +56,6 @@ export class Table extends ExcelComponent {
         this.selection.select($cell)
         this.$emit('table:select', $cell)
         const styles = $cell.getStyles(Object.keys(defaultStyles))
-        console.log('styles to dispatch', styles)
         this.$dispatch(actions.changeStyles(styles))
     }
 
